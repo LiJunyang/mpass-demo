f3a3dbb563574d00d8fe18ea444e7a4f40268cd6
JiangyanLi <lijiangyan@bigo.sg>
2022-11-23 18:56:17 +0800

    add static bg to me tab

--- app/src/main/java/com/hsbcd/mpaastest/kotlin/samples/ui/activity/me/MeFragment.kt
+++ app/src/main/java/com/hsbcd/mpaastest/kotlin/samples/ui/activity/me/MeFragment.kt
@@ -2,9 +2,14 @@
  * Ant Group
  * Copyright (c) 2004-2022 All Rights Reserved.
  */
-package com.hsbcd.mpaastest.kotlin.samples.ui.activity.me;
+package com.hsbcd.mpaastest.kotlin.samples.ui.activity.me
 
-import androidx.fragment.app.Fragment;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import cn.hsbcsd.mpaastest.databinding.FragmentMeBinding
 
 /**
  * 我的信息tab页
@@ -12,6 +17,14 @@ import androidx.fragment.app.Fragment;
  * @author liyalong
  * @version MessageFragment.java, v 0.1 2022年07月29日 15:53 liyalong
  */
-public class MeFragment extends Fragment {
-
+class MeFragment : Fragment() {
+    private lateinit var binding: FragmentMeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
