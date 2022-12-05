68cd7a1c5ea2d79b97639d5050ab07028fda9890
HSBC_AUTO <>
2022-12-05 14:18:17 +0800

    add picture selector

--- app/src/main/java/com/hsbcd/mpaastest/kotlin/samples/ui/activity/discover/PostDiscoverActivity.kt
+++ app/src/main/java/com/hsbcd/mpaastest/kotlin/samples/ui/activity/discover/PostDiscoverActivity.kt
@@ -1,11 +1,19 @@
 package com.hsbcd.mpaastest.kotlin.samples.ui.activity.discover
 
+import android.content.Intent
+import android.graphics.drawable.Icon
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import cn.hsbcsd.mpaastest.R
 import cn.hsbcsd.mpaastest.databinding.ActivityPostDiscoverBinding
+import com.hsbcd.mpaastest.kotlin.samples.util.GlideEngine
+import com.luck.picture.lib.basic.PictureSelector
+import com.luck.picture.lib.config.SelectMimeType
+import com.luck.picture.lib.entity.LocalMedia
+import com.luck.picture.lib.interfaces.OnResultCallbackListener
+
+
 class PostDiscoverActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPostDiscoverBinding
@@ -24,12 +32,30 @@ class PostDiscoverActivity : AppCompatActivity() {
                 binding.ivPost.isEnabled = true
             } else {
                 binding.ivPost.setImageResource(R.mipmap.bg_send_post)
-                binding.ivPost.isEnabled = true
+                binding.ivPost.isEnabled = false
             }
         }
 
         binding.ivPost.setOnClickListener {
             finish()
         }
+
+        binding.ivPostDiscoverBottom.setOnClickListener {
+
+            PictureSelector.create(this)
+                .openGallery(SelectMimeType.ofImage())
+                .setImageEngine(GlideEngine.createGlideEngine())
+                .forResult(object : OnResultCallbackListener<LocalMedia?> {
+                    override fun onResult(result: ArrayList<LocalMedia?>?) {
+                        binding.postImage.setImageIcon(Icon.createWithFilePath(result?.get(0)?.availablePath
+                            ?: null))
+                    }
+                    override fun onCancel() {}
+                })
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
     }
 }
